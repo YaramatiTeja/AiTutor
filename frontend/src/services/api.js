f338be75d06c8edc1850222f63ea7f44af3e152a
YaramatiTeja <yaramatiteja@gmail.com>
2026-07-19 22:39:17 +0530

    Configured production API Url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
